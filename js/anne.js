2a95d87022fadb4a626249844631dc5a5bd62ee5
XINYU714 <62043697+XINYU714@users.noreply.github.com>
2022-04-17 00:06:47 +0800

    Delete anne.js

--- js/anne.js
+++
@@ -1,8 +0,0 @@
-const anneani = bodymovin.loadAnimation({
-  container: document.getElementById("anne"),
-  path: "js/anne.json",
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-});
-anneani.setSpeed(0.5);
